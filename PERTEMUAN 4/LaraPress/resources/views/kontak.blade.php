75d8bbec623481a9a400869ed6809763096fa8c2
Fitria04 <fitriadwi990@gmail.com>
2025-10-17 15:14:12 +0700

    menambahkan folder

---
+++ PERTEMUAN 4/LaraPress/resources/views/kontak.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+<title>Kontak - LaraPress</title> 
+</head> 
+<body> 
+<h1>Kontak LaraPress</h1> 
+<p>Nomor Telepon : 08999999999</p> 
+<br>
+<p>Email : www@gmail</p> 
+</body> 
+<a href="/">Kembali ke Halaman Utama</a> 
+</html>
---
+++ PERTEMUAN 4/LaraPress/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create'); 
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store'); 
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
